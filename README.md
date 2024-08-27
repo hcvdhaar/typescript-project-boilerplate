0586fc09e6f1029c0db8daef789988e7d36fa2e4
hvdhaar <h.c.vanderhaar@gmail.com>
2024-08-27 15:23:34 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Typesscript Starter Boilerplate
+
+Basic Typescript Boilerplate Template for starting a Typescript project.
+This is template is using [TSUP](https://tsup.egoist.dev/) for bundling and building Typescript. You get a lot free out of the box.
+
+Futhermore, there is a basic ESLint and prettier config.
+
+Create a copy of this template and run:
+
+```bash
+pnpm install
+```
---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/*'],
+  },
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {},
+  },
+);
